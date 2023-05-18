9c8843a836a5c406330c1f5aa1b083495d1686cd
v1gnesh <v1gnesh@users.noreply.github.com>
2023-05-18 07:50:33 -0400

    fix newline

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/direnvport.git'), string(name: 'PORT_DESCRIPTION', value: 'Load and unload env. variables and envs[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[C[3~[[[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C depending on the current directory' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/direnvport.git'), string(name: 'PORT_DESCRIPTION', value: 'Load and unload env. variables and envs, depending on the current directory' )]
   }
 }
