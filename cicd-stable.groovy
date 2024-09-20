0bb7b15e1017fa40a2d819525edb31d88e27fb3c
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:27 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/direnvport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/direnvport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/direnvport.git'), string(name: 'PORT_DESCRIPTION', value: 'direnv' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/direnvport.git'), string(name: 'PORT_DESCRIPTION', value: 'direnv' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
   }
 }
